413660b167f6b096d40824582b9e86416d553fb0
Bellangelo <aggelosbellos7@gmail.com>
2025-09-07 01:46:52 +0300

    Add auth annotations

--- src/Server/NotificationPublisher.php
+++ src/Server/NotificationPublisher.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the official PHP MCP SDK.
+ *
+ * A collaboration between Symfony and the PHP Foundation.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Mcp\Server;
 
 use Mcp\Event\PromptListChangedEvent;
@@ -15,6 +24,9 @@ use Mcp\Schema\Notification\ToolListChangedNotification;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Contracts\EventDispatcher\Event;
 
+/**
+ * @author Aggelos Bellos <aggelosbellos7@gmail.com>
+ */
 class NotificationPublisher
 {
     /**
