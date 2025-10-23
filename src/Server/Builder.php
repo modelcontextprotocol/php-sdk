3aef606628c315b5188d3ea84bca61f99c5a8fd5
Illia Vasylevskyi <ineersa@gmail.com>
2025-10-23 16:27:50 -0400

    Ability to set custom protocol version

--- src/Server/Builder.php
+++ src/Server/Builder.php
@@ -27,6 +27,7 @@ use Mcp\Capability\Registry\ReferenceHandler;
 use Mcp\Exception\ConfigurationException;
 use Mcp\JsonRpc\MessageFactory;
 use Mcp\Schema\Annotations;
+use Mcp\Schema\Enum\ProtocolVersion;
 use Mcp\Schema\Implementation;
 use Mcp\Schema\Prompt;
 use Mcp\Schema\PromptArgument;
@@ -74,6 +75,8 @@ final class Builder
     private int $paginationLimit = 50;
 
     private ?string $instructions = null;
+
+    private ?ProtocolVersion $protocolVersion = null;
 
     /**
      * @var array<int, RequestHandlerInterface>
@@ -293,6 +296,13 @@ final class Builder
         return $this;
     }
 
+    public function setProtocolVersion(?ProtocolVersion $protocolVersion): self
+    {
+        $this->protocolVersion = $protocolVersion;
+
+        return $this;
+    }
+
     /**
      * Manually registers a tool handler.
      *
@@ -391,7 +401,7 @@ final class Builder
         $messageFactory = MessageFactory::make();
 
         $capabilities = $registry->getCapabilities();
-        $configuration = new Configuration($this->serverInfo, $capabilities, $this->paginationLimit, $this->instructions);
+        $configuration = new Configuration($this->serverInfo, $capabilities, $this->paginationLimit, $this->instructions, $this->protocolVersion);
         $referenceHandler = new ReferenceHandler($container);
 
         $requestHandlers = array_merge($this->requestHandlers, [
