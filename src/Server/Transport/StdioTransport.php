6d8cfe14539d5503fa4fae300c57cdd37b17aaf4
Kyrian Obikwelu <koshnawaza@gmail.com>
2025-12-27 15:52:35 +0100

    fix: revert changes to server related components

--- src/Server/Transport/StdioTransport.php
+++ src/Server/Transport/StdioTransport.php
@@ -157,7 +157,7 @@ class StdioTransport extends BaseTransport
 
     private function writeLine(string $payload): void
     {
-        fwrite($this->output, $payload . \PHP_EOL);
+        fwrite($this->output, $payload.\PHP_EOL);
     }
 
     public function close(): void
